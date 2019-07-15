36ff87772c4062672bd222b89b592e53991d8326
pavel.sivkovych <pavel.sivkovych@worldapp.com>
2019-07-15 20:07:32 +0300

    Move properties parsing/validation to run function. Add hooks.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,9 +9,43 @@ export default class Form extends Component<IFormSettings, IFormState> {
             age: settings.age,
             name: settings.name
         };
+        console.log("From [Form::constructor]");
+    }
+
+    public componentWillMount(): void {
+        console.log("From [Form::componentWillMount]");
+    }
+
+    public componentWillUnmount(): void {
+        console.log("From [Form::componentWillMount]");
+    }
+
+    public getChildContext(): object {
+        console.log("From [Form::componentWillMount]");
+        return {
+            i_am: "child_context"
+        };
+    }
+
+    public componentWillReceiveProps(): void {
+        console.log("From [Form::componentWillMount]");
+    }
+
+    public shouldComponentUpdate(): boolean {
+        console.log("From [Form::componentWillMount]");
+        return true;
+    }
+
+    public componentDidUpdate(): void {
+        console.log("From [Form::componentDidUpdate]");
+    }
+
+    public componentWillUpdate(): void {
+        console.log("From [Form::componentWillUpdate]");
     }
 
     public componentDidMount(): void {
+        console.log("From [Form::componentDidMount]");
         setTimeout(() => {
             this.setState({
                 name: "React's componentDidMount worked as expected"
@@ -20,6 +54,12 @@ export default class Form extends Component<IFormSettings, IFormState> {
     }
 
     public render(settings: IFormSettings, state: IFormState): ComponentChild {
-        return <h1>props: {settings.name}, state: {state.name}, age: {state.age}</h1>;
+        console.log("From [Form::render]");
+        return (
+            <h1>
+                settings: {JSON.stringify(settings, null, "\t")},
+                state: {JSON.stringify(state, null, "\t")}
+            </h1>
+        );
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,20 @@ import Form from "./components/Form";
 import {IFormSettings} from "./model/IFormSettings";
 
 // noinspection JSUnusedGlobalSymbols
-export default function renderForm(targetElement: Element, settings: IFormSettings): void {
-    render((
-        <Form {...settings}/>
-    ), targetElement);
+export default function renderMe(settings: IFormSettings | undefined | null): void {
+    if (typeof settings !== "object" || settings === null) {
+        console.warn("Configuration is not defined.");
+        return;
+    }
+    const rootSelector = settings.rootSelector;
+    if (typeof rootSelector !== "string") {
+        console.warn("Mandatory configuration parameter [rootSelector] is empty.");
+        return;
+    }
+    const $root: Element | null = document.querySelector(rootSelector);
+    if ($root === null) {
+        console.warn("Could not find root element by [rootSelector] configuration parameter.");
+        return;
+    }
+    render(<Form {...settings}/>, $root);
 }
